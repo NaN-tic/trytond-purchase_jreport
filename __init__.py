7dafa1b4201d552fc4fb8a44152c6d20377dc71c
Bernat Brunet Torruella <bernat@nan-tic.com>
2016-01-14 21:23:37 +0100

    Add config decimals use and refact part of report

--- __init__.py
+++ __init__.py
@@ -2,10 +2,15 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 from trytond.pool import Pool
+from .configuration import *
 from .purchase import *
 
 
 def register():
+    Pool.register(
+        Configuration,
+        PurchaseConfigurationCompany,
+        module='purchase_jreport', type_='model'),
     Pool.register(
         PurchaseReport,
         PurchaseRequestReport,
--- purchase.py
+++ purchase.py
@@ -1,7 +1,7 @@
 # This file is part purchase_jreport module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.pool import PoolMeta
+from trytond.pool import Pool, PoolMeta
 from trytond.modules.jasper_reports.jasper import JasperReport
 
 __all__ = ['PurchaseReport', 'PurchaseRequestReport']
@@ -11,6 +11,34 @@ __metaclass__ = PoolMeta
 class PurchaseReport(JasperReport):
     __name__ = 'purchase.purchase'
 
+    @classmethod
+    def execute(cls, ids, data):
+        pool = Pool()
+        Config = pool.get('purchase.configuration')
+        config = Config(1)
+        parameters = {
+            'purchase_qty_decimal': config.purchase_qty_decimal
+            }
+        if 'parameters' in data:
+            data['parameters'] += parameters
+        else:
+            data['parameters'] = parameters
+        return super(PurchaseReport, cls).execute(ids, data)
+
 
 class PurchaseRequestReport(JasperReport):
     __name__ = 'purchase.purchase_request'
+
+    @classmethod
+    def execute(cls, ids, data):
+        pool = Pool()
+        Config = pool.get('purchase.configuration')
+        config = Config(1)
+        parameters = {
+            'purchase_qty_decimal': config.purchase_qty_decimal
+            }
+        if 'parameters' in data:
+            data['parameters'] += parameters
+        else:
+            data['parameters'] = parameters
+        return super(PurchaseRequestReport, cls).execute(ids, data)
